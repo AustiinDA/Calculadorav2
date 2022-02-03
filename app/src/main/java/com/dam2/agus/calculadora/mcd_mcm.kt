e4561ebc7a559046feb339f21761c3025b6ef87c
AustiinDA <agustindialoi@gmail.com>
2022-02-03 13:05:08 +0100

    avance 2

--- app/src/main/java/com/dam2/agus/calculadora/mcd_mcm.kt
+++ app/src/main/java/com/dam2/agus/calculadora/mcd_mcm.kt
@@ -20,80 +20,47 @@ class mcd_mcm : AppCompatActivity() {
         val resul2 = findViewById<TextView>(R.id.textoResul2)
 
 
-        val resultadoMcm = resul1.text.toString()
-
-
-
-
-
-
-        btnCal.setOnClickListener {
-            val valor1 = num1.text.toString().toInt()
-            val valor2 = num2.text.toString().toInt()
-
-
-            calcularMcd(valor1, valor2)
-
-        }
-
-         fun calcularMcd(num1: Int, num2: Int): String {
+        fun calcularMcm(num1: Int, num2: Int) {
+            var resultadoMcm: Int
             var mcm = if (num1 > num2) {
                 num1
-            }else num2
+            } else num2
 
             while (true) {
                 if (mcm % num1 == 0 && mcm % num2 == 0) {
 
-                     resultadoMcm = "$num1 y $num2 es $mcm"
-
+                    resultadoMcm = mcm
 
                     break
                 }
                 mcm++
             }
+            resul2.text = resultadoMcm.toString()
         }
 
-    }
+        fun calcularMcd(num1: Int, num2: Int) {
+            var resultadoMcd: Int = 0
+            var cont: Int = 1;
 
-
-
-    /*fun calcularMcd() {
-        val num1Formateado: Int = num1.text.toString().toInt()
-        var listaResultados: MutableList<Int> = mutableListOf()
-        var index = 0
-        resultado = num1Formateado
-
-        for (numero in numPrimos) {
-
-            var primoEncontrado: Int = numPrimos[index]
-
-            if (resultado.mod(numPrimos[index]) == 0) {
-                listaResultados.add(primoEncontrado)
-                resul1.text = listaResultados.toString()
-
-            } else {
-                index++
+            while (cont <= num1 && cont <= num2) {
+                if (num1 % cont == 0 && num2 % cont == 0) {
+                    resultadoMcd = cont
+                }
+                cont++
             }
-
-            resultado.div(numPrimos[index])
+            resul1.text = resultadoMcd.toString()
 
         }
-    val n1 = 72
-    val n2 = 120
-    var lcm: Int
 
-    // maximum number between n1 and n2 is stored in lcm
-    lcm = if (n1 > n2) n1 else n2
+        btnCal.setOnClickListener {
+            val valor1 = num1.text.toString().toInt()
+            val valor2 = num2.text.toString().toInt()
 
-    // Always true
-    while (true) {
-        if (lcm % n1 == 0 && lcm % n2 == 0) {
-            println("The LCM of $n1 and $n2 is $lcm.")
-            break
+            calcularMcm(valor1, valor2)
+            calcularMcd(valor1,valor2)
         }
-        ++lcm
+
     }
 
-        */
 
 }
