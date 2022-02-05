54f2a3af0ef02df2a4445717760a8b1b6edf4094
Austin_DA <agustindialoi@gmail.com>
2022-02-05 03:31:31 +0100

    menu lateral implementado

--- app/src/main/java/com/dam2/agus/calculadora/CalculadoraDeterminantes.kt
+++ app/src/main/java/com/dam2/agus/calculadora/CalculadoraDeterminantes.kt
@@ -7,24 +7,15 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 
 class CalculadoraDeterminantes : AppCompatActivity() {
-    lateinit var toggle: ActionBarDrawerToggle
-    private val drawerLayout: DrawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora_determinantes)
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item))
-            true
-        return super.onOptionsItemSelected(item)
-    }
+
 }
--- app/src/main/java/com/dam2/agus/calculadora/MainActivity.kt
+++ app/src/main/java/com/dam2/agus/calculadora/MainActivity.kt
@@ -3,13 +3,19 @@ package com.dam2.agus.calculadora
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
 
     private lateinit var mostrarCalculo: TextView
     var sumado: Boolean = false
@@ -24,17 +30,53 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar);
 
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
-        val btnProAvz = findViewById<Button>(R.id.btnPro)
+        val navVista: NavigationView = findViewById(R.id.navVista)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
 
-        btnProAvz.setOnClickListener{
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navVista.setNavigationItemSelectedListener {
             val intent = Intent(this, mcd_mcm::class.java)
-            startActivity(intent)
+            when (it.itemId) {
+                R.id.miItem1 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked item1 ", Toast.LENGTH_SHORT
+                ).show()
+                R.id.miItem2 ->
+                    startActivity(intent)
+                R.id.miItem2 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked item2 ", Toast.LENGTH_SHORT
+                ).show()
+                R.id.miItem3 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked item3 ", Toast.LENGTH_SHORT
+                ).show()
+            }
+            true
         }
-
         mostrarCalculo = findViewById(R.id.mostrarCalculoTV)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            true
+        return super.onOptionsItemSelected(item)
+
+
+    // val intent = Intent(this, mcd_mcm::class.java)
+       // startActivity(intent)
+    }
+
 
     private fun actualizarTextoNum(lineaAInsertar: String) {
         var lineaOld: String = mostrarCalculo.text.toString()
